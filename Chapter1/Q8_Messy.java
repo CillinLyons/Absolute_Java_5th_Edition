a3c4e07e7904107a36290aff198e952e3c3248c0
CillinLyons <cillinlyons@outlook.com>
2016-09-19 17:17:55 +0100

    Update Q8_Messy.java

--- Chapter1/Q8_Messy.java
+++ Chapter1/Q8_Messy.java
@@ -7,18 +7,22 @@ naming conventions, and formatting style recommended in this book.
 
 public class Q8_Messy 
 {
+	//Declaring constant varible
 	public static final double DISTANCE = 6.21;
 	
 	public static void main(String[] args)
 	{
-		double time; 
-		double pace;
+		//Declaring double variables
+		double time,
+			pace;
 		
 		System.out.println("This program calculates your pace given a time and distance traveled.");
 		
+		//Calculating pace
 		time = 35.5; /* 35 minutes and 30 seconds */
 		pace = time / DISTANCE;
 		
+		//Printing the results
 		System.out.println("Your pace is " + pace + " miles per hour.");
 	}
 	
