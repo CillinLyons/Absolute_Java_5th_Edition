551c0e28e67fd54b9ef8e02e4e341a9e5f5c3506
CillinLyons <cillinlyons@outlook.com>
2016-09-19 17:21:45 +0100

    Update Q9_IdealWeight.java

--- Chapter1/Q9_IdealWeight.java
+++ Chapter1/Q9_IdealWeight.java
@@ -14,46 +14,56 @@ import java.util.Scanner;
 
 public class Q9_IdealWeight
 {
+	//Declaring constant variables
 	public static final double FIVE_FOOT_WEIGHT = 110,
-								ADDITIONAL_INCHE_WEIGHT = 5,
-								FIVE_FOOT_IN_INCHES = 60;
+					ADDITIONAL_INCHE_WEIGHT = 5,
+					FIVE_FOOT_IN_INCHES = 60;
 	public static final int ONE_FOOT = 12;
 	
 	public static void main(String[] args)
 	{
+		//Declaring scanner object
 		Scanner userInput = new Scanner(System.in);
 		
+		//Declaring variables
 		int heightFeet;
-		double heightInches;
-		double feetToInches;
-		double totalHeightInInches;
-		double weightInFeet;
-		double weightInInches;
-		double personsWeight;
+		double heightInches, 
+			feetToInches,
+			totalHeightInInches,
+			weightInFeet,
+			weightInInches,
+			personsWeight;
 		
+		//Enter and store height in feet
 		System.out.println("Enter height in feet: ");
 		heightFeet = userInput.nextInt();
 		
+		//Enter and store remaining inches
 		System.out.println("Enter remaining inches: ");
 		heightInches = userInput.nextInt();
 		
+		//Sending values to the appropriate method for calculations
 		feetToInches = convert_feet_to_inches(heightFeet);
 		totalHeightInInches = calculate_total_height_inches(feetToInches, heightInches);
 		personsWeight = calculate_weight(totalHeightInInches);
 		
+		//Printing the results
 		System.out.println("Your ideal weight is: " + personsWeight + "pounds");
 	}
 	
+	//Converting the feet to inches
 	public static int convert_feet_to_inches(int feet)
 	{
 		return feet * ONE_FOOT;
 	}
 	
+	//Calculating the total height in inches
 	public static double calculate_total_height_inches(double remainingInches, double feetInInches)
 	{
 		return remainingInches + feetInInches;
 	}
 	
+	//Calculating the weight
 	public static double calculate_weight(double totalInches)
 	{
 		
