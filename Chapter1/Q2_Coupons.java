2bf34dc915522c818b844dd7aad4d061a714bc48
CillinLyons <cillinlyons@outlook.com>
2016-09-19 16:38:23 +0100

    Update Q2_Coupons.java

--- Chapter1/Q2_Coupons.java
+++ Chapter1/Q2_Coupons.java
@@ -5,39 +5,45 @@ for a gumball. You prefer candy bars to gumballs. Write a program that defines a
 */
 import java.util.Scanner;
 
-class Q3_Coupons
+class Q2_Coupons
 {
-	public static final int CANDY_BAR = 10;
-	public static final int GUM_BALL = 3;
+	//Declaring constant variables
+	public static final int CANDY_BAR = 10,
+				GUM_BALL = 3;
 	
 	public static void main(String[] args)
 	{
+		//Declaring scanner object
 		Scanner user_input = new Scanner(System.in);
 		
-		int couponsWon;
-		
+		//Enter and store number of coupons
 		System.out.print("Enter the amount of coupons you have won: ");
-		couponsWon = user_input.nextInt();
+		int couponsWon = user_input.nextInt();
 		
+		//Send values to methods to calculate resutls
 		int totalCandyBars = CalculateTotalCandyBars(couponsWon);
 		int remainder = CalculateCouponRemainder(couponsWon);
 		int totalGumBalls = CalculateTotalGumBalls(remainder);
 		
+		//Print results
 		System.out.println("You can receive " + totalCandyBars + " candy bars.");
 		System.out.println("&");
 		System.out.println("You can receive " + totalGumBalls + " gum balls.");
 	}
 	
+	//Method to calculate the total number of candy bars
 	private static int CalculateTotalCandyBars(int coupon)
 	{
 		return coupon/CANDY_BAR;
 	}
 	
+	//Method to calculate the remaining number of coupons
 	private static int CalculateCouponRemainder(int coupon)
 	{
 		return coupon%CANDY_BAR;
 	}
 	
+	//Method to calculate the total number of gum balls
 	private static int CalculateTotalGumBalls(int coupon)
 	{
 		return coupon/GUM_BALL;
